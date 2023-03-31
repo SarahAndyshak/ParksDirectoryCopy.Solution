7ce0e1937167de387df5dc6d5f8cb17cfd7b1a67
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-03-31 10:37:39 -0700

    Successfully tested all CRUD functions.

---
+++ ParksDirectory/Migrations/20230331173658_AddUpdateAfterTesting.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParksDirectory.Migrations
+{
+    public partial class AddUpdateAfterTesting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

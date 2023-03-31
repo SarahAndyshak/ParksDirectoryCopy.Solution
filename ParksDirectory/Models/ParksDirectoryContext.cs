cf7552684b3fc97e9645fa5f33c8db678cc9cb71
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-03-31 09:19:37 -0700

    Seed database

--- ParksDirectory/Models/ParksDirectoryContext.cs
+++ ParksDirectory/Models/ParksDirectoryContext.cs
@@ -14,16 +14,15 @@ namespace ParksDirectory.Models
     {
       builder.Entity<Park>()
         .HasData(
-          new Park {},
-          new Park {},
-          new Park {},
-          new Park {},
-          new Park {},
-          new Park {},
-          new Park {},
-          new Park {},
-          new Park {},
-          new Park {}
+          new Park { ParkId = 1, Name = "Crater Lake", Classification = "national park", Location = "Oregon", MajorLandmarks = "Crater Lake, Wizard Island", YearFounded = 1902 },
+          
+          new Park { ParkId = 2, Name = "Newberry National Volcanic Monument", Classification = "national monument", Location = "Oregon", MajorLandmarks = "Lava Butte, Lava River Cave, Lava Cast Forest, Newberry Caldera, Big Obsidian Flow, Paulina Peak, East Lake, Paulina Lake", YearFounded = 1990 },
+          
+          new Park { ParkId = 3, Name = "Smith Rock State Park", Classification = "state park", Location = "Oregon", MajorLandmarks = "Smith Rock", YearFounded = 1960 },
+          
+          new Park { ParkId = 4, Name = "Tumalo State Park", Classification = "state park", Location = "Oregon", MajorLandmarks = "Deschutes River Trail", YearFounded = 1954 },
+
+          new Park { ParkId = 5, Name = "Deschutes National Forest", Classification = "national forest", Location = "Oregon", MajorLandmarks = "Mount Jefferson, Mount Washington, the Three Sisters, Lava Lands, Metolius River, Cascade Lakes", YearFounded = 1908 }
         );
     }
   }
